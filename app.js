7b083ef825413dcfe238dd288aff33840ccd5877
teenabobeena <christinas.codingemail@gmail.com>
2025-05-14 16:03:05 -0400

    Add button and click event

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const container = document.querySelector("div");
+const body = document.querySelector("body");
 
+const btn = document.createElement("button");
+body.appendChild(btn);
+btn.textContent = "Reset";
 
 
 for (let i = 1; i <= 16; i++) {
@@ -7,3 +11,15 @@ for (let i = 1; i <= 16; i++) {
     square.classList.add("box");
     container.appendChild(square);
 }
+
+const gridSquares = document.querySelectorAll(".box");
+
+gridSquares.forEach(square => {
+    square.addEventListener("mouseover", function() {
+        square.style.backgroundColor = "blue";
+    });
+});
+
+btn.addEventListener("click", () => {
+    prompt("How many squares?");
+})
