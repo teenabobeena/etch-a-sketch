1bce819791c375313239d1b792bcfe0e18f598ed
teenabobeena <christinas.codingemail@gmail.com>
2025-05-15 16:24:37 -0400

    Add board logic for reset

--- app.js
+++ app.js
@@ -1,25 +1,40 @@
-const container = document.querySelector("div");
+const container = document.querySelector(".container");
 const body = document.querySelector("body");
 
 const btn = document.createElement("button");
 body.appendChild(btn);
 btn.textContent = "Reset";
 
+function makeGrid(count) {
+    container.innerHTML = "";
+    const gridSize = Math.ceil(Math.sqrt(count));
+    const boxSize = 500 / gridSize;
 
-for (let i = 1; i <= 16; i++) {
-    const square = document.createElement("div");
-    square.classList.add("box");
-    container.appendChild(square);
+    for (let i = 0; i < count; i++) {
+        const square = document.createElement("div");
+        square.classList.add("box");
+        square.style.width = `${boxSize}px`;
+        square.style.height = `${boxSize}px`;
+        container.appendChild(square);
+    }
+
+    const gridSquares = document.querySelectorAll(".box");
+
+    gridSquares.forEach(square => {
+        square.addEventListener("mouseover", function() {
+            square.style.backgroundColor = "blue";
+        });
+    });
 }
 
-const gridSquares = document.querySelectorAll(".box");
-
-gridSquares.forEach(square => {
-    square.addEventListener("mouseover", function() {
-        square.style.backgroundColor = "blue";
-    });
-});
+makeGrid(16);
 
 btn.addEventListener("click", () => {
-    prompt("How many squares?");
+    const userCount = parseInt(prompt("How many squares? Max 99"));
+    
+    while (isNaN(userCount) || userCount < 1 || userCount > 100) {
+        userCount = parseInt(prompt("Please enter a number between 1 and 100"));
+    }
+
+    makeGrid(userCount);
 })
